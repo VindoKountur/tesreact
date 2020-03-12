b9044e926f507a045e00ace1f8a182830ef3bae7
Vindo Kountur <vindokountur@gmail.com>
2020-03-12 20:01:21 +0800

    Updates

---
+++ precache-manifest.14f90bed7aa60acca4f8881f4128c4d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66badd28b386c62e0c0148cf1e44da42",
+    "url": "/tesreact/index.html"
+  },
+  {
+    "revision": "78231b430a93fe187927",
+    "url": "/tesreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bdf285ba76babdc0eadc",
+    "url": "/tesreact/static/js/2.e8bd01d6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tesreact/static/js/2.e8bd01d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78231b430a93fe187927",
+    "url": "/tesreact/static/js/main.acf3ea96.chunk.js"
+  },
+  {
+    "revision": "221c4d4cf06285ab7b8e",
+    "url": "/tesreact/static/js/runtime-main.0a53e5cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tesreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
