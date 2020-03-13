a8b8ed7f948d38882bd77546705d4f8a3a353394
Vindo Kountur <vindokountur@gmail.com>
2020-03-13 09:34:23 +0800

    Change template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [theme, setTheme] = useState(true);
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  let getQuotes = () => {
+    axios.get("https://quotes.rest/qod?language=en").then(res => {
+      // console.log(res.data.contents.quotes);
+      setTitle(res.data.contents.quotes[0].title);
+      setQuote(res.data.contents.quotes[0].quote);
+      setAuthor(res.data.contents.quotes[0].author);
+    });
+  };
+
+  let changeTheme = () => {
+    setTheme(!theme);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Today Change
-        </a>
+    <div className={(theme) ? "light" : "dark"}>
+      <header className="main-header">
+        <p className="title"><strong>{title}</strong></p>
       </header>
+      <main>
+        <h1 className="quote">{quote}</h1>
+        <p className="author"><em>" {author} "</em></p>
+        <button className="switch-btn" onClick={changeTheme}><strong>{(theme) ? "Dark Theme" : "Light Theme"}</strong></button>
+      </main>
     </div>
   );
 }
